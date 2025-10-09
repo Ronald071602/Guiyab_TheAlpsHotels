5e2b40e9d2f0d78b7030efd1edf336e5a4d817b0
Ronald Guiyab <ronaldguiyab4@gmail.com>
2025-10-09 20:06:38 +0800

    Homapage Layout
    Created Homapage's General layout and loaded hotels from json file

--- app/src/main/java/ph/edu/comteq/thealpshotel/MainActivity.kt
+++ app/src/main/java/ph/edu/comteq/thealpshotel/MainActivity.kt
@@ -4,14 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.google.gson.Gson
 import ph.edu.comteq.thealpshotel.ui.theme.TheAlpsHotelTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +55,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheAlpsHotelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Homepage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +65,152 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Homepage(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var hotels by remember { mutableStateOf(emptyList<Hotel>()) }
+    var searchQuery by remember { mutableStateOf("") }
+
+    val filteredHotels = hotels.filter {
+        it.hotel_name.contains(searchQuery, ignoreCase = true)
+    }
+    // load json data
+    LaunchedEffect(Unit) {
+        val json = context.assets.open("hotels.json")
+            .bufferedReader()
+            .use { it.readText() }
+        val gson = Gson()
+        val hotelsArray = gson.fromJson(json, Array<Hotel>::class.java)
+        hotels = hotelsArray.toList()
+    }
+
+    // main container
+    Column(
         modifier = modifier
-    )
+    ) {
+        // header
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // left side: title and logo
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "The Alps Hotel",
+                    modifier = Modifier.padding(end = 8.dp),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.france_national_flag),
+                    contentDescription = "France Flag",
+                    modifier = Modifier.width(40.dp)
+                )
+            }
+            // right side: user icon
+            Icon(
+                imageVector = Icons.Outlined.Person,
+                contentDescription = "User Icon",
+                modifier = Modifier.width(40.dp)
+            )
+        }
+
+        // search bar
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            placeholder = { Text("Search...") },
+            singleLine = true,
+        )
+
+        // hotel list
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(filteredHotels) { hotel ->
+//                Text(
+//                    text = hotel.hotel_name,
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Bold,
+//                    modifier = Modifier.padding(16.dp)
+//                )
+                HotelCard(hotel = hotel)
+            }
+        }
+    }
 }
 
+@Composable
+fun HotelCard(hotel: Hotel) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Hotel Image
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("file:///android_asset/${hotel.hotel_cover_image}")
+                    .crossfade(true)
+                    .build(),
+                contentDescription = hotel.hotel_name,
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                modifier = Modifier.size(120.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+            // hotel info
+            Column(
+                modifier = Modifier.weight(1f).padding(horizontal = 12.dp)
+            ) {
+                Text(
+                    text = hotel.hotel_name,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = hotel.hotel_rating.toString(),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    // star
+                        // TODO :
+                    repeat(4){
+                        Icon(
+                            imageVector = Icons.Outlined.Star,
+                            contentDescription = "Star",
+                            modifier = Modifier.size(16.dp),
+                            tint = Color(0xFFFFD700)
+                        )
+                    }
+                }
+                Text (
+                    text = "${hotel.hotel_to_ski_distance} km to ski",
+                    fontSize = 14.sp,
+                    color = Color.Gray
+                )
+            }
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomepagePreview() {
     TheAlpsHotelTheme {
-        Greeting("Android")
+        Homepage(modifier = Modifier)
     }
 }
